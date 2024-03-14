0d1f49dddf92a9ca9ec299602706928f98788be2
ArvaDev <armandorovi1590@gmail.com>
2024-03-14 17:54:46 -0600

    Carrusel añadido

--- src/components/UI/carrusel/Carrusel.jsx
+++ src/components/UI/carrusel/Carrusel.jsx
@@ -1,22 +1,39 @@
 import './Carrusel.css'
+import { useState } from 'react';
+
 import Producto from "../producto/Producto";
 
-function Carrusel() {
+const componentes = [
+    <Producto nombre="Cafe" precio="32.5"/>,
+    <Producto nombre="Miel" precio="16.5"/>,
+    <Producto nombre="Pox" precio="43.0"/>,
+    <Producto nombre="Pimienta" precio="6.0"/>,
+    <Producto nombre="Té" precio="20.0"/>,
+]
+
+export default function Carrusel() {
+
+    const [cardIndex, setCardIndex] = useState(0);
+
+    const previusCard = () => {
+        const newIndex = Math.max(0, cardIndex - 1);
+        setCardIndex(newIndex);
+    };
+
+    const goToNextSlide = () => {
+        const newIndex = Math.min(cardIndex + 1, componentes.length - 4);
+        setCardIndex(newIndex);
+      };
+
     return (
         <div className='CarruselClass'>
-            <button>{'<'}</button>
+            <button onClick={previusCard}>{'<'}</button>
             <div className='Container'>
-                <Producto/>
-                <Producto/>
-                <Producto/>
-                <Producto/>
-                <Producto/>
-                <Producto/>
-                <Producto/>
+                {
+                    componentes.length === 0 ? <p>No hay productos</p> : componentes.slice(cardIndex, cardIndex + 4)
+                }
             </div>
-            <button>{'>'}</button>
+            <button onClick={goToNextSlide}>{'>'}</button>
         </div>
     );
 }
-
-export default Carrusel;
--- src/components/UI/producto/Producto.jsx
+++ src/components/UI/producto/Producto.jsx
@@ -1,18 +1,16 @@
 import './Producto.css'
 import img1 from '../../../assets/imgs/m12.jpeg'
 
-function Producto({image, nombre, precio}) {
+export default function Producto({image, nombre, precio}) {
     return (
         <div className='ProductoClass'>
             <div className='ContainerImg'>
                 <img src={img1}></img>
             </div>
             <div className='Container'>
-                <p>Cafe claro</p>
-                <span>$12.5</span>
+                <p>{nombre}</p>
+                <span>${precio}</span>
             </div>
         </div>
     );
 }
-
-export default Producto;
