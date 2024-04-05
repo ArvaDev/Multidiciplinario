e24c6b48093eb0a9f49137fdd7843aaeaee2555e
ArvaDev <armandorovi1590@gmail.com>
2024-04-05 16:30:59 -0600

    Responsivo

--- src/components/UI/carrusel/Carrusel.jsx
+++ src/components/UI/carrusel/Carrusel.jsx
@@ -2,27 +2,22 @@ import './Carrusel.css'
 import { useState } from 'react';
 
 export default function Carrusel({ componentes }) {
-
     const [cardIndex, setCardIndex] = useState(0);
-    const cardsToShow = 2;
-
+    console.log(window.innerWidth)
+    const cardsToShow = window.innerWidth <= 1243 ? 2 : 4;
     const previusCard = () => {
         const newIndex = Math.max(0, cardIndex - 1);
         setCardIndex(newIndex);
     };
-
     const goToNextSlide = () => {
         const newIndex = Math.min(cardIndex + 1, componentes.length - cardsToShow);
         setCardIndex(newIndex);
     };
-
     return (
         <div className='CarruselClass'>
             <button onClick={previusCard}>{'<'}</button>
             <div className='Container'>
-                {
-                    componentes.length === 0 ? <p>No hay productos</p> : componentes.slice(cardIndex, cardIndex + cardsToShow)
-                }
+                { componentes.length === 0 ? <p>No hay productos</p> : componentes.slice(cardIndex, cardIndex + cardsToShow) }
             </div>
             <button onClick={goToNextSlide}>{'>'}</button>
         </div>
